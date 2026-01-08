2ca7cd622005b439959933b4d980b0a6e3f241ca
Carlos <cpovedasanjose@gmail.com>
2026-01-08 13:24:23 +0100

    Ejemplo básico ODB

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,30 @@
 package org.example;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class Main {
     static void main() {
         Game game = new Game("ngvirsbv","descrip","girsbv",1000);
         EntityManager entityManager = DataProvider.getEntityManagerFactory().createEntityManager();
+//        try {
+//            entityManager.getTransaction().begin();
+//            // SAVE
+//            entityManager.persist(game);
+//
+//            entityManager.getTransaction().commit();
+//        }
+//        finally {
+//            entityManager.close();
+//        }
+
+        // FIND ALL
         try {
-            entityManager.getTransaction().begin();
-
-            entityManager.persist(game);
-
-            entityManager.getTransaction().commit();
+            TypedQuery<Game> query = entityManager.createQuery("select g from Game g",Game.class);
+            List<Game> games = query.getResultList();
+            for (Game g : games) {
+                System.out.println(g);
+            }
         }
         finally {
             entityManager.close();
